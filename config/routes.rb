f4e61eda84d30145ac56bcda25ee241d9d514286
Mst.oki <bbbar326@gmail.com>
2018-09-02 18:42:30 +0900

    XML upload æ©è½è¿½å 

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :receipt_details
   resources :receipts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'xml_upload', to: 'receipts#xml_upload'
 end
