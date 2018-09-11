fe993fa6e8960d824e9786a0814c4155c6fd45c7
Mst.oki <bbbar326@gmail.com>
2018-09-11 20:46:48 +0900

    TOPページに月次サマリ追加

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,10 @@
 class RootController < ApplicationController
   def index
+    # 先々月の合計値
+    @last_last_month_summary = Receipt.joins(:receipt_details).where(date: Time.current.last_month.last_month.all_month).sum(:price)
+    # 先月の合計値
+    @last_month_summary = Receipt.joins(:receipt_details).where(date: Time.current.last_month.all_month).sum(:price)
+    # 今月の合計値
+    @this_month_summary = Receipt.joins(:receipt_details).where(date: Time.current.all_month).sum(:price)
   end
 end
