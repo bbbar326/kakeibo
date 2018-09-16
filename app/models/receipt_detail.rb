129a3b8fafb28175322c461316727a63863b0507
Mst.oki <bbbar326@gmail.com>
2018-09-16 22:55:35 +0900

    経費種別毎のサマリ追加

--- app/models/receipt_detail.rb
+++ app/models/receipt_detail.rb
@@ -1,11 +1,4 @@
 class ReceiptDetail < ApplicationRecord
   belongs_to :receipt
   belongs_to :expense, optional: true
-
-=begin
-  def expense
-#    @expense ||= Expense.new
-    Expense.new if self.expense.blank?
-  end
-=end
 end
