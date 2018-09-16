68c9f1fd3cd0b2479658874bbe066e593bf6e0ec
Mst.oki <bbbar326@gmail.com>
2018-09-16 22:55:35 +0900

    経費種別毎のサマリ追加

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,10 +1,24 @@
 class RootController < ApplicationController
   def index
+    # ---------------------------------------------------------
+    # 経費種別合計値
+    # ---------------------------------------------------------
+    # 先々月の経費別合計値
+    @last_last_month_expenses_sum = Receipt.joins(:receipt_details).where(date: Time.current.last_month.last_month.all_month).group(:expense_id).sum(:price)
+    # 先月の経費別合計値
+    @last_month_expenses_sum = Receipt.joins(:receipt_details).where(date: Time.current.last_month.all_month).group(:expense_id).sum(:price)
+    # 今月の経費別合計値
+    @this_month_expenses_sum = Receipt.joins(:receipt_details).where(date: Time.current.all_month).group(:expense_id).sum(:price)
+
+    # ---------------------------------------------------------
+    # 合計
+    # ---------------------------------------------------------
     # 先々月の合計値
-    @last_last_month_summary = Receipt.joins(:receipt_details).where(date: Time.current.last_month.last_month.all_month).sum(:price)
+    @last_last_month_sum = @last_last_month_expenses_sum.values.sum
     # 先月の合計値
-    @last_month_summary = Receipt.joins(:receipt_details).where(date: Time.current.last_month.all_month).sum(:price)
+    @last_month_sum = @last_month_expenses_sum.values.sum
     # 今月の合計値
-    @this_month_summary = Receipt.joins(:receipt_details).where(date: Time.current.all_month).sum(:price)
+    @this_month_sum = @this_month_expenses_sum.values.sum
+
   end
 end
--- app/helpers/receipts_helper.rb
+++ app/helpers/receipts_helper.rb
@@ -1,16 +1,2 @@
 module ReceiptsHelper
-
-  #
-  # ストア名, ストアID の一覧を取得します
-  #
-  def store_list
-    @store_list ||= Store.all.order(:id).pluck(:name, :id)
-  end
-
-  #
-  # 経費種別名, 経費種別ID の一覧を取得します
-  #
-  def expense_list
-    @expense_list ||= Expense.all.order(:id).pluck(:name, :id)
-  end
 end
