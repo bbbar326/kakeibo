1abb57d56b57d7a6627089f9fdc39984e5bbd09b
Mst.oki <bbbar326@gmail.com>
2018-09-19 09:23:34 +0900

    レシート検索機能を追加

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -3,4 +3,6 @@ class Receipt < ApplicationRecord
   belongs_to :pay_account, optional: true
   has_many :receipt_details, dependent: :delete_all
   accepts_nested_attributes_for :receipt_details
+
+  scope :search, lambda { |search_word| where("date LIKE ?", "%#{search_word}%") }
 end
