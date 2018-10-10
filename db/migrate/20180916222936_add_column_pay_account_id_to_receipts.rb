363305e4277cce922a2103e53d97452e635cf40c
Mst.oki <bbbar326@gmail.com>
2018-10-10 23:13:39 +0900

    カラムの追加位置を修正

--- db/migrate/20180916222936_add_column_pay_account_id_to_receipts.rb
+++ db/migrate/20180916222936_add_column_pay_account_id_to_receipts.rb
@@ -1,6 +1,6 @@
 class AddColumnPayAccountIdToReceipts < ActiveRecord::Migration[5.0]
   def up
-    add_column :receipts, :pay_account_id, :integer
+    add_column :receipts, :pay_account_id, :integer, after: :store_id
   end
 
   def down
