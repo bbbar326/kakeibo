9c1ddac5b0d6f9c9e1224c35b73d7e6bc46955f7
Mst.oki <bbbar326@gmail.com>
2019-07-30 16:59:46 +0900

    modelの修正が即時反映されるように

--- rails/config/environments/development.rb
+++ rails/config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  reload_classes_only_on_change = false  # 追加(https://qiita.com/uenoryo/items/5dd69436e9f5af6a752e)
 
   # Do not eager load code on boot.
   config.eager_load = false
