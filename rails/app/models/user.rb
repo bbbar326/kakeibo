e641f145c11da3d5b262236db133f454fd575818
Mst.oki <bbbar326@gmail.com>
2019-10-10 01:13:55 +0900

    ログイン周りの仕様が固まるまで、簡単にサインアップできないようにする

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+  #TODO 仕様が固まるまで、簡単にサインアップできないようにする
+  #  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
+:recoverable, :rememberable, :validatable
 end
